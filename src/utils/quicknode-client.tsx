3fc43bb583471eb1cfcff642c6b27363f8353359
albert <thatguyintech@gmail.com>
2023-04-14 21:21:40 +0900

    fix getLogs inputs

--- src/utils/quicknode-client.tsx
+++ src/utils/quicknode-client.tsx
@@ -1,4 +1,4 @@
-import { providers } from "ethers";
+import { BigNumber, providers } from "ethers";
 import keccak256 from "keccak256";
 
 const provider = new providers.JsonRpcProvider(
@@ -26,16 +26,17 @@ export const getWalletTokenTransactions = async (
 
 export const getLogs = async (
   topic: string, // only supports one topic for now
-  address: string,
-  fromBlock: string,
-  toBlock: string, 
+  contractAddress: string,
+  walletAddress: string,
+  fromBlock: number, // i.e. 123456
+  toBlock: number,  // i.e. 123456
 ) => {
   const topicHex = keccak256(topic).toString("hex");
   const logs = await provider.getLogs({
-    address: address || "0xdf9eb223bafbe5c5271415c75aecd68c21fe3d7f",
-    fromBlock: fromBlock || "latest",
-    toBlock: toBlock || "latest",
-    topics: [`0x${topicHex}`],
+    address: contractAddress,
+    fromBlock: BigNumber.from(fromBlock).toHexString() || "latest",
+    toBlock: BigNumber.from(toBlock).toHexString() || "latest",
+    topics: [`0x${topicHex}`, walletAddress],
   });
   console.log(logs);
   return logs;
